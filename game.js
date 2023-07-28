76ccdf630965bd35c41e9bf4ef574b1642ad27e5
untserius <sudhirdas1999@gmail.com>
2023-07-28 22:14:01 +0530

    DOM funtionality added

--- game.js
+++ game.js
@@ -2,52 +2,77 @@
 const box = ["rock", "paper", "scissors"];
 let userScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 // function to get computer's choice
-getComputerChoice = () => {
-    const randomIndex =  Math.floor(Math.random() * box.length);
+const getComputerChoice = () => {
+    const randomIndex = Math.floor(Math.random() * box.length);
     return box[randomIndex];
-}
-
-// reassign computer's choice
-computerSelection = getComputerChoice();
-
-// loop to play 5 rounds
-for (let i = 1; i < 5; i++) {
-    playerSelection = prompt("Enter your move");
-    computerSelection = getComputerChoice();
-    alert(playGame(playerSelection, computerSelection));
-}
-
-// to determine the winner
-if (userScore > computerScore) {
-    alert("Congratulations! You scored " + userScore + " points and the computer scored " + computerScore + " points." + " You Win!");
-} else if (userScore < computerScore) {
-    alert("Sorry! You scored " + userScore + " points and the computer scored " + computerScore + " points." + " You Lose!");
-} else {
-    alert("Its a Tie! Both scored " + userScore + " points.");
-}
+};
 
 // function to play the game
 function playGame(playerSelection, computerSelection) {
-    
-    const userChoice = playerSelection.toLowerCase();
-    const computerChoice = computerSelection.toLowerCase();
+    const userChoice = playerSelection;
+    const computerChoice = computerSelection;
 
     if (userChoice === computerChoice) {
-        return "Its a Tie!";
+        return "It's a Tie!";
     } else if (
-        userChoice === "rock" && computerChoice === "scissors" ||
-        userChoice === "paper" && computerChoice === "rock" ||
-        userChoice === "scissors" && computerChoice === "paper"
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
     ) {
         userScore++;
+        updateScore();
         return "You Win! " + userChoice + " beats " + computerChoice;
     } else {
         computerScore++;
+        updateScore();
         return "You Lose! " + computerChoice + " beats " + userChoice;
     }
 }
 
+// function to update scores
+function updateScore() {
+    document.getElementById("userScore").innerHTML = userScore;
+    document.getElementById("computerScore").innerHTML = computerScore;
+}
+
+// function to reset the game
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    roundCount = 0;
+    updateScore();
+    document.getElementById("output").innerHTML = "";
+}
+
+// function to handle the game logic after each round
+function handleRound(playerSelection) {
+    if (roundCount < 5) {
+        const computerSelection = getComputerChoice();
+        let result = playGame(playerSelection, computerSelection);
+        document.getElementById("output").innerHTML = result;
+        roundCount++;
+    }
+
+    if (roundCount === 5) {
+        endGame();
+    }
+}
+
+function endGame() {
+    document.getElementById("output").innerHTML = "Final Result: " + 
+    (userScore > computerScore) ?
+        "Congratulations! You scored " + userScore + " points and the computer scored " + computerScore + " points." + " You Win!" : 
+        "Sorry! You scored " + userScore + " points and the computer scored " + computerScore + " points." + " You Lose!";
+}
+
+// game functions
+document.querySelector("#rock").addEventListener("click", () => handleRound("rock"));
+document.querySelector("#paper").addEventListener("click", () => handleRound("paper"));
+document.querySelector("#scissor").addEventListener("click", () => handleRound("scissors"));
 
 
+// reset game
+document.querySelector("#reset").addEventListener("click", () => resetGame());
